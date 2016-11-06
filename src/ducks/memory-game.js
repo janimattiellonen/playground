071620ddd505c5ae710b29aaa8a7f567be7a4e29
Janimatti Ellonen <janimatti.ellonen@gmail.com>
2016-11-06 11:57:44 +0200

    fuktores

--- src/ducks/memory-game.js
+++ src/ducks/memory-game.js
@@ -1,8 +1,9 @@
 import { List, Map } from 'immutable';
 
-export const RECEIVE_MEMORY_SQUARES = 'RECEIVE_MEMORY_SQUARES';
-export const FLIP_SQUARE            = 'FLIP_SQUARE';
-export const RESET_FLIPS            = 'RESET_FLIPS';
+export const RECEIVE_MEMORY_SQUARES     = 'RECEIVE_MEMORY_SQUARES';
+export const FLIP_SQUARE                = 'FLIP_SQUARE';
+export const RESET_FLIPS                = 'RESET_FLIPS';
+export const MARK_SQUARES_AS_MATCHING   = 'MARK_SQUARES_AS_MATCHING';
 
 const defaultState = Map({
     memorySquares: List(),
@@ -19,6 +20,7 @@ export function receiveMemorySquares() {
             url: require('../images/animals/rabbits/rabbit.jpg'),
             sound: require('../sounds/animals/rabbits/rabbit.mp3'), 
             flipped: false,
+            matches: false,
         });
 
         squares.push({
@@ -27,6 +29,7 @@ export function receiveMemorySquares() {
             url: require('../images/animals/tigers/tiger.jpg'),
             sound: require('../sounds/animals/tigers/tiger.mp3'), 
             flipped: false,
+            matches: false,
         });
 
         squares.push({
@@ -35,6 +38,7 @@ export function receiveMemorySquares() {
             url: require('../images/animals/horses/horse.jpg'),
             sound: require('../sounds/animals/horses/horse.mp3'), 
             flipped: false,
+            matches: false,
         });
 
         let id = 4;
@@ -46,6 +50,7 @@ export function receiveMemorySquares() {
                 url: require('../images/animals/cats/nova.jpg'),
                 sound: require('../sounds/animals/cats/cat.mp3'), 
                 flipped: false,
+                matches: false,
             });
 
             squares.push({
@@ -54,8 +59,11 @@ export function receiveMemorySquares() {
                 url: require('../images/animals/dogs/30220942672_8afaa23661_b.jpg'),
                 sound: require('../sounds/animals/dogs/Dogs-Barking-SoundBible.com-625577590.mp3'),
                 flipped: false,
+                matches: false,
             });
         }
+
+        console.log(JSON.stringify(squares.map((t) => {return t.id;})));
 
         return dispatch({
             type: RECEIVE_MEMORY_SQUARES,
@@ -71,14 +79,51 @@ export function selectSquare(id) {
 
         console.log(JSON.stringify(getState().memoryGame.get('memorySquares').get(1), null, 4));
 
-        if (getState().memoryGame.get('memorySquares').filter(s => s.flipped === true).count() >= 2) {
+        const flipped = getState().memoryGame.get('memorySquares').filter(s => s.flipped === true);
+
+        console.log("flip count: " + flipped.count());
+
+        if (flipped.count() == 1) {
+
+            const toBeFlipped = getState().memoryGame.get('memorySquares').get(getState().memoryGame.get('memorySquares').findIndex(t => t.id === id));
+
+            console.log(id + ", 0: " + flipped.get(0).squareId);
+            console.log(id + ", 1: " + toBeFlipped.squareId);
+
+            dispatch({
+                type: FLIP_SQUARE,
+                payload: id,
+            });  
+
+            if (flipped.get(0).squareId === toBeFlipped.squareId) {
+                console.log("WE HAVE A MATCH!");
+                dispatch({
+                    type: MARK_SQUARES_AS_MATCHING,
+                    payload: [
+                        flipped.get(0).squareId,
+                        toBeFlipped.squareId,
+                    ],
+                });  
+            } else {
+                 setTimeout(function(){ 
+                        dispatch({
+                        type: RESET_FLIPS,
+                    });  
+                }, 1000);
+
+                
+            } 
+            return;
+
+        } else if (flipped.count() >= 2) {
             console.log("no more flippin");
+
             return dispatch({
                 type: RESET_FLIPS,
             });  
         }
 
-
+        console.log("Reached the end, now flipping");
         return dispatch({
             type: FLIP_SQUARE,
             payload: id,
@@ -104,9 +149,39 @@ export default function (state = defaultState, action) {
         case RESET_FLIPS:
             console.log("ss");
             return state.update('memorySquares', squares => squares.map((t) => {
-                t.flipped = false;
+                if (t.matches === false) {
+                   t.flipped = false; 
+                }
+                
                 return t;
             }));
+        case MARK_SQUARES_AS_MATCHING:
+            const id1 = action.payload[0];
+            const id2 = action.payload[1];
+
+            let foo = state
+                .updateIn(
+                    [
+                        'memorySquares',
+                        state.get('memorySquares').findIndex(t => t.id === id1)
+                    ], square => ({
+                        ...square,
+                        flipped: true,
+                        matches: true,
+                    })
+                )
+
+            return foo
+                .updateIn(
+                    [
+                        'memorySquares',
+                        state.get('memorySquares').findIndex(t => t.id === id2)
+                    ], square => ({
+                        ...square,
+                        flipped: true,
+                        matches: true,
+                    })
+                )
 
         default:
             return state;
